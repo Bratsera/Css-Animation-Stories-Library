39ce6c0e6f67aaa5f5a7708199314f7cfb57aa3a
Arthur Scheiermann <83575368+Bratsera@users.noreply.github.com>
2023-09-28 17:11:03 +0200

    Initial commit

---
+++ src/lib/Buttons/AnimatedButton.svelte
@@ -0,0 +1,31 @@
+<a href="">Hover me</a>
+<style>
+    a{
+        text-decoration: none;
+        color: #272727;
+        font-family: sans-serif;
+        font-size: 45px;
+        border: 5px solid #272727;
+        padding: 40px 80px;
+        position: relative;
+
+        overflow: hidden;
+    }
+    a:before{
+        transform-origin: bottom left;
+        content: '';
+        position: absolute;
+        left: 0;
+        top: 0;
+        height: 100%;
+        width:100%;
+        transform: rotate(-90deg);
+        background-color: #fff200;
+        z-index: -1;
+        transition: transform 1s;
+  
+    }
+    a:hover:before{
+        transform: rotate(0);
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import AnimatedButton from "$lib/Buttons/AnimatedButton.svelte";
+</script>
+<div class="" style="display: flex; justify-content:center; align-items: center;">
+    <AnimatedButton></AnimatedButton>
+
+</div>
