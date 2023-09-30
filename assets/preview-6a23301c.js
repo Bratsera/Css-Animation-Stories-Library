a685164f145db1259502a9baf93a1f5642b5cc1b
Arthur Scheiermann <83575368+Bratsera@users.noreply.github.com>
2023-09-30 11:12:16 +0200

    Updates

---
+++ assets/preview-6a23301c.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-1f48e4ef.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-3PUGWF3O-f5973188.js"),["./DocsRenderer-3PUGWF3O-f5973188.js","./iframe-1f48e4ef.js","./index-39079268.js","./_commonjsHelpers-de833af9.js","./index-d37d4223.js","./index-e04ae519.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-6a23301c.js.map
